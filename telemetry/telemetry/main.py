0280d79b83f3f09a06a78ca6a167d74f12c20088
Jacky Zhao <j.zhao2k19@gmail.com>
2024-07-21 21:51:49 -0700

    try subscription mode

--- telemetry/telemetry/main.py
+++ telemetry/telemetry/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import influxdb_client, os
 from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+import datetime
 
 load_dotenv()
 
@@ -16,40 +17,43 @@ org = "magicarp"
 bucket="vehicle"
 url = "https://magicarp-telemetry.fly.dev"
 
-# commands: List[obd.OBDCommand] = [
-#   obd.commands.RPM
-# ]
+commands: List[obd.OBDCommand] = [
+  obd.commands.RPM
+]
+
+drive_id = f"drive-{datetime.today().isoformat()}"
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+def create_influx_callback(command: obd.OBDCommand):
+  def callback(response):
+    if response.value == None or not isinstance(response.value, obd.Unit.Quantity):
+      return
+    val = response.value.magnitude
+
+    point = Point(command.name).field("value", val).tag("drive", drive_id)
+    write_api.write(bucket=bucket, org="magicarp", record=point)
+    print(f"wrote {command.name}={val} to influx")
+
+  return callback
 
 def start():
-    client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-    write_api = client.write_api(write_options=SYNCHRONOUS)
+  print("connecting to obd2 port")
+  connection = obd.Async(
+    delay_cmds=0,
+    fast=True,
+  )
 
-    print("connecting to obd2 port")
-    connection = obd.OBD()
+  if not connection.is_connected():
+    print("connection failed")
+    return
 
-    if not connection.is_connected():
-      print("connection failed")
-      return
+  print(f"connected using {connection.protocol_name()}, listing available commands: ")
+  print(connection.print_commands())
 
-    print(f"connected using {connection.protocol_name()}")
-    print(connection.print_commands())
 
-    commands = connection.supported_commands
+  for command in commands:
+    print(f"watching {command.name}")
+    connection.watch(command, callback=create_influx_callback(command))
 
-    # query loop
-    while True:
-      points = []
-      for cmd in commands:
-        response = connection.query(cmd)
-        if response.value == None:
-          continue
-        if not isinstance(response.value, obd.Unit.Quantity):
-          continue
-
-        val = response.value.magnitude
-
-        point = Point(cmd.name).field("value", val)
-        points.append(point)
-      
-      print(f"wrote {len(points)} points to influx")
-      write_api.write(bucket=bucket, org="magicarp", record=points)
+  connection.start()
